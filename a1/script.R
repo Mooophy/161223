32b29b0c92e22e5c2d1dc19a264c417cc87f2b82
Yue Wang <yue.wang.programmer@gmail.com>
2018-07-16 21:52:04 +1200

    Update script.R

--- a1/script.R
+++ a1/script.R
@@ -1,6 +1,6 @@
-climate <- read.table(file="http://www.massey.ac.nz/~mhazelto/161223/data/climate.txt",header=T)
+climate <- read.table(file="http://www.massey.ac.nz/~mhazelto/161223/data/climate.txt", header=T)
 
-year.full = climate[,1] - 1 + 1863
+year.full = climate[, 1] - 1 + 1863
 climate["full"] <- year.full
 climate
-barplot(climate[,5]) # todo: labels + down to 1919
+barplot(climate[, 5]) # todo: labels + down to 1919
