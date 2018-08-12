8b1956fa5ce3c83362544424f8d1c93ac9dee648
Yue Wang <yue.wang.programmer@gmail.com>
2018-08-12 15:04:51 +1200

    Update script.R

--- a1/script.R
+++ a1/script.R
@@ -21,3 +21,11 @@ FirstObservationAtB$Month
 # estimates of the mean temperature by month in both locations C and D
 tapply(climate$C.temp, climate$Month, mean)
 tapply(climate$D.temp, climate$Month, mean)
+
+# E1.1.3
+barplot(
+  climate[ climate$FullYear == 1919 ,]$C.temp, 
+  main = "Monthly Temperatures At Location C In 1919", 
+  xlab = "Months", ylab = "Temperatures", 
+  names.arg = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+)
