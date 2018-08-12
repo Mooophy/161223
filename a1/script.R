b42dcc2e271d30c507ae03b92a0c722e3fc84c05
Yue Wang <yue.wang.programmer@gmail.com>
2018-08-12 14:42:36 +1200

    Update script.R

--- a1/script.R
+++ a1/script.R
@@ -1,24 +1,22 @@
 climate <- read.table(file="http://www.massey.ac.nz/~mhazelto/161223/data/climate.txt", header=T)
 
+climate["FullYear"] <- climate[, 1] - 1 + 1863
 
 # E1.1.i
 # the last observation was made at location A is 1924, March as shown below
 
 LastObservationAtA <- tail(climate[!is.na(climate$A.temp),], 1)
-LastObservationAtA$Year - 1 + 1863
+LastObservationAtA$FullYear
 LastObservationAtA$Month
 
 # E1.1.ii
 # the first observation was made at location B is 1924, January as shown below
 
 FirstObservationAtB <- head(climate[!is.na(climate$B.temp),], 1)
-FirstObservationAtB$Year - 1 + 1863
-LastObservationAtA$Month
+FirstObservationAtB$FullYear
+FirstObservationAtB$Month
 
 # E1.1.2
 # estimates of the mean temperature by month in both locations C and D
 tapply(climate$C.temp, climate$Month, mean)
 tapply(climate$D.temp, climate$Month, mean)
-
-
-barplot(climate[, 5]) # todo: labels + down to 1919
