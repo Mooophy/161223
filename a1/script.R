5339655445fd6f62f83e8b3954e55e6280e2ecee
Yue Wang <yue.wang.programmer@gmail.com>
2018-08-12 15:05:54 +1200

    Update script.R

--- a1/script.R
+++ a1/script.R
@@ -23,7 +23,6 @@ tapply(climate$C.temp, climate$Month, mean)
 tapply(climate$D.temp, climate$Month, mean)
 
 # E1.1.3
-# E1.1.3
 barplot(
   climate[ climate$FullYear == 1919 ,]$C.temp, 
   main = "Monthly Temperatures At Location C In 1919", 
